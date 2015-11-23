887f7b2facbf275435f9708f672be84eb4b14ccd
Dante <dant.e@tutanota.com>
2015-11-22 16:07:44 -0800

    FC

---
+++ ex77.c
@@ -0,0 +1,33 @@
+/* Write a function that raises an integer to a postive integer power.
+ *  *      Call the function x_to_the_n taking two integer arguments x and n.
+ *   *      Have the function return a long int, which represents the results
+ *    *      of calculating x.                                               */
+ 
+#include <stdio.h>
+ 
+int main (void)
+{
+        long x;
+        int  i;
+ 
+        printf ("This program will calculate x to the nth power.\n");
+        printf ("Choose an x > 0: ");
+        scanf  ("%li", &x);
+ 
+        for ( i = 0; i < 3; ++i ) {
+            if ( x <= 0 ) {
+                printf ("BITCH I SAID LESS THAN 0.\n");
+                printf ("Try again bitch ass motherfucking: ");
+                scanf  ("%li", &x);
+            }
+            else {
+                break;
+            }
+        }      
+ 
+        if ( x <= 0 ) {
+            printf ("You are a fucking bitch.\n");
+            return 1;
+        }
+}
+//      x_to_the_n();
