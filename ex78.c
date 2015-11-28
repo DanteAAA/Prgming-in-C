cf938e855ae01fa8183f86e38a9007e0bd371848
Alphonse <al@fake.com>
2015-11-27 21:12:33 -0800

    Finished?

--- ex78.c
+++ ex78.c
@@ -2,8 +2,7 @@
 #include <stdio.h>
 
 // Global Variables
-int a, b, c;
-
+int a, b, c, x1, x2;
 
 void get_variables (void)
 {
@@ -36,21 +35,24 @@ float squareRoot (int x)
     return guess;
 }
 
-int quad_fml(void)
+void quad_fml(int a, int b, int c)
 {
-    int bb sqrt;
+    int bb, sqrt, top, t1, t2;
 
     bb = b * b;
     sqrt = bb * -4 * a * c;
-    squareRoot(sqrt);
-
-    return result;
+    top = squareRoot(sqrt);
+    t1 = -b + top;
+    t2 = -b - top;
+    x1 = t1 / 2a;
+    x2 = t2 / 2a;
 }
 
 int main (void)
 {
     get_variables();
-    printf ("The quadratic solutions are:\nx = %i, x = %i.\n", a, quad_fml());
+    quad_fml(a, b, c);
+    printf ("The quadratic solutions are:\nx = %i, x = %i.\n", x1, x2);
 
     return 0;
 }
