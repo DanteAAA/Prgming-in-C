848af429c63a76b0c5326241a751f6374d999895
Alphonse <al@fake.com>
2015-11-27 17:18:16 -0800

    global variables

--- ex77.c
+++ ex77.c
@@ -1,18 +1,26 @@
 // This program calculates x to the nth power
 
-int (get_variables)
-{
-	static int x;
-	static int n;
+#include <stdio.h>
 
+// Global Variables since I don't know how to only allow them
+// in functions, and not globally
+
+	int i;
+	int x;
+	int n;
+
+int get_variables (void)
+{
 	printf ("What is your x: ");
 	scanf  ("%i", &x);
 
 	printf ("What is your n: ");
 	scanf  ("%i", &n);
+
+	return (x, n);
 }
 
-long unsigned x_to_the_n (void)
+long unsigned x_to_the_n (int x, int n)
 {
 	for ( i = 0; i < n; ++i )
 	    x *= x;
@@ -20,13 +28,17 @@ long unsigned x_to_the_n (void)
 	return x;
 }
 
+void print_output (void)
+{
+	printf ("%i to the %i power is: %lu", x, n, x_to_the_n());
+}
 
 int main (void)
 {
 	get_variables();
-	x_to_the_n();
+	x_to_the_n(x, n);
 
-	printf ("%i to the %i power is: %lu", x, n, x_to_the_n());
+	print_output();
 
 	return 0;
 }
