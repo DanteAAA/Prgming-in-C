1327faa55e4726f0045c8c14935245accc27e2ff
Alphonse <al@fake.com>
2015-12-02 03:18:16 -0800

    MISSION COMPLETE!

--- ex711.c
+++ ex711.c
@@ -7,10 +7,11 @@
 
 arraySum(int rayman[], int x)
 {
-    int i, total;
+    int i, total = 0;
 
     for ( i = 0; i < x; ++i ) {
         total += rayman[i];
+        printf ("%i: %i\n", i, total);
     }
 
     return total;
@@ -21,11 +22,13 @@ int main (void)
     int n, numOfElements = 5;
     int array[4];
 
-    for ( n = 0; n >= numOfElements; ++n ) {
-        array[n] = 5;
+    // 0 out array[4]
+    for ( n = 0; n <= numOfElements; ++n ) {
+       array[n] = 5;
+       printf ("array[%i]: %i\n", n, array[n]);
     }
 
-    printf("The total of the array is: %i\n", arraySum(array[4], numOfElements));
+    printf("The total of the array is: %i\n", arraySum(array, numOfElements));
 
     return 0;
 }
