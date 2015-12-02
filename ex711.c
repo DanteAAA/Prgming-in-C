b4d60ef3cc6229af4d0c4bf66eb03f568f75e952
Alphonse <al@fake.com>
2015-12-02 01:09:14 -0800

    FC

---
+++ ex711.c
@@ -0,0 +1,31 @@
+/* Write a function called arraySum() that takes two
+ * arguments: an integer array and the number of elements
+ * in the array. Have the function return as its result
+ * the sum of the elemnts in the array.                     */
+
+#inlcude <stdio.h>
+
+arraySum(int rayman[], int x)
+{
+    int i, total;
+
+    for ( i = 0; i < x; ++i ) {
+        total += rayman[i];
+    }
+
+    return total;
+}
+
+int main (void)
+{
+    int n, numOfElements = 5;
+    int array[4];
+
+    for ( n = 0; n >= numOfElements; ++n ) {
+        array[i] = 5;
+    }
+
+    printf("The total of the array is: %i\n", arraySum(array[4], numOfElements));
+
+    return 0;
+}
