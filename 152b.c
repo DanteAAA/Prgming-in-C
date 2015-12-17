e7fe030c4ee7ccbe484b5107f8a5521f8f116142
Dante <dant.e@tutanota.com>
2015-12-16 18:39:53 -0800

    Fixed the for loop. There seems to be some difference in the language or complier from mine to Ritchie's book

--- 152b.c
+++ 152b.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 /* count characters in input; 1st version */
-main()
+int main (void)
 {
     double nc;
 
     for (nc = 0; getchar() != EOF; ++nc)
-	;
-    printf("%.0f\n", nc);
+        printf("%.0f\n", nc);
 }
