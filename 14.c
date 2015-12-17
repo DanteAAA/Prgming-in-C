d245ffc8653865dc0a047a6134540c16948ade41
Dante <dant.e@tutanota.com>
2015-12-16 18:07:54 -0800

    Fixed the variable

--- 14.c
+++ 14.c
@@ -6,10 +6,10 @@
 #define  STEP   20      // step size
 
 // print Fahrenheit-Celsius table 
-main()
+int main (void)
 {
     int fahr;
 
     for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
-        printf("%3d %6. 1f\n", fahr, (5.0/9.0)*(fahr-32));
+        printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
 }
